d0a2d8bdb070c7c818df905c42bf978d4ce46778
Humberto Ham <hamduranhumberto@gmail.com>
2022-10-30 21:19:09 -0600

    no intro

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import 'bootstrap/dist/css/bootstrap.css';
 import NavbarM from '../components/NavbarM'
 import Foot from '../components/Foot';
-import Intro from '../components/intro';
+
 import Banner from '../components/Banner';
 import Best from '../components/Best';
 import LookMenu from '../components/LookMenu';
 import LookFacebook from '../components/LookFacebook';
 import CarF from '../components/CarF';
-import Poster from '../components/Poster';
+
 
 import Contact from '../components/Contact';
 import Locations from '../components/Locations';
