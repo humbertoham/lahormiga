956e03fcf3ccaf2fc29bbb5082d2d83f73377305
Humberto Ham <hamduranhumberto@gmail.com>
2022-12-18 23:28:21 -0600

    locations

--- components/Locations.js
+++ components/Locations.js
@@ -6,7 +6,7 @@ function  Locations() {
       <div class="col-md-6">
                             <h1 className="textL">UBICACIONES </h1>
                             <h4 className="subL">Contamos con múltiples locaciones en Reynosa, Tamp.</h4>
-                                <a id="verL" href="https://www.google.com/maps/place/C.+Pek%C3%ADn+110,+Las+Fuentes+Coloniales,+88703+Reynosa,+Tamps./@26.0606379,-98.3239508,15z/data=!4m13!1m7!3m6!1s0x8665058cba544423:0x9a6cb0fb9683683a!2sC.+Pek%C3%ADn+110,+Las+Fuentes+Coloniales,+88703+Reynosa,+Tamps.!3b1!8m2!3d26.0606379!4d-98.3151961!3m4!1s0x8665058cba544423:0x9a6cb0fb9683683a!8m2!3d26.0606379!4d-98.3151961">Locaciones</a>
+                                <a id="verL" href="/ubicaciones">LOCACIONES</a>
                         </div>
         <div class="col-md-6 ">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229445.07874118641!2d-98.58330842544936!3d26.030959437787562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866509e70b1ab591%3A0x4568c85f5cba4884!2sReynosa%2C%20Tamaulipas!5e0!3m2!1sen!2smx!4v1671216960545!5m2!1sen!2smx" className="map"></iframe>
